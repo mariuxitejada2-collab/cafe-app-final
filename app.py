c4c108852176a813d0ec71a4b4dd507a5e31d802
mariuxitejada2-collab <mariuxitejada2@gmail.com>
2026-03-12 15:15:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -27,9 +27,7 @@ SHEET_NAME = "DB_CAFE"
 url = f"https://docs.google.com/spreadsheets/d/1Qx6Uhz_XHSETKhQwlgYNpaenq6-8nTKfGcbwAvL7hkg/edit?usp=sharing"
 
 @st.cache_data(ttl=5) # Se actualiza rápido para pruebas
-# Cambia la línea 30 por esta (mira que agregamos &headers=1 al final):
-# COPIA ESTA LÍNEA EXACTA (Línea 31):
-url = f"https://docs.google.com{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}&headers=1"
+url = f"https://docs.google.com/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}&headers=1"
     # El motor 'python' es más robusto para errores de red
     return pd.read_csv(url, on_bad_lines='skip', engine='python')
 
@@ -64,5 +62,6 @@ try:
 except Exception as e:
     st.error(f"Error al leer la hoja: {e}")
     st.info("Revisa que la pestaña en tu Google Sheet se llame exactamente: DB_CAFE")
+
 
 
